59a0bbc6c4ac040380085ef536040a804fbb9404
ianb <ian246@hotmail.co.uk>
2024-05-08 22:24:06 +0100

    Added startNewGame function

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,9 +1,20 @@
 
-//assign span to hold timer to timer variable
-let timer = document.getElementById('countdownTimer')
+//function to start new game
+function startNewGame() {
+    //start the countdown
+    setTimeout(countdown, 1000);
+};
+
+
+//event listener for start game button
+let startButton = document.getElementById('startGame');
+    startButton.addEventListener('click', startNewGame);
+
 
 //countdown function for players time to complete game 
 function countdown() {
+    //assign span to hold timer to timer variable
+    let timer = document.getElementById('countdownTimer')
     //assign variable seconds to the inner text content of countdowntimer span
     let seconds = parseInt(timer.innerText);
     //start countdown if not already at 0
@@ -12,9 +23,10 @@ function countdown() {
         seconds--;
         //assign new value of seconds to span
         timer.innerText = seconds;
+        //call the countdown function again after 1 second
+        setTimeout(countdown, 1000);
     };
 };
 
 
 
-setInterval(countdown, 1000);
