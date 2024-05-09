80f94d313e2902a4721784c150d426b799599292
ianb <ian246@hotmail.co.uk>
2024-05-09 21:57:12 +0100

    Added generateLetters function

--- assets/scripts.js
+++ assets/scripts.js
@@ -28,5 +28,27 @@ function countdown() {
     };
 };
 
+//function to generate 12 random letters
+function generateLetters() {
+    //an array holding consonants
+    let consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p',
+     'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'];
+    //an array holding vowels
+    let vowels = ['a', 'e', 'i', 'o', 'u'];
+    //randomly choose 4 vowels and 8 consonants and add to letters array
+    let letters = [];
+    //for loop to add 4 random vowels to array 
+    for (let i = 0; i < 4; i ++) { 
+        //
+        letters.push(vowels[Math.floor(Math.random() * vowels.length)]);
+    }
+    //for loop to add 8 random consonants to the array
+    for (let i = 0; i < 8 ; i++) {
+        letters.push(consonants[Math.floor(Math.random() * consonants.length)]);
+    }
+    return letters;
+};
 
 
+console.log(generateLetters());
+ 
